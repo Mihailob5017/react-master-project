6b4e97ff158b8eeb9054c15b536947e6c596b9c1
Mihailo Boskovic <mihailob5017@gmail.com>
2020-02-17 00:28:11 +0100

    added the homepage and some basic styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import './style/App.css';
+//  Import Components
+import Homepage from './components/homepage/Homepage';
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 };
 
 export default App;
