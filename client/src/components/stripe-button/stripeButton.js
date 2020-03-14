26198ad588336c8945e635460172135fff03feca
Mihailo Boskovic <mihailob5017@gmail.com>
2020-03-14 19:09:04 +0100

    added a backedn & integrated stripe

--- client/src/components/stripe-button/stripeButton.js
+++ client/src/components/stripe-button/stripeButton.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 const stripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publisherKey = 'pk_test_GAxMvOOUijRiVr54YStpKILC00xqXhxiWS';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(res => {
+        alert('Payment Successful');
+      })
+      .catch(err => {
+        console.log(JSON.parse(err));
+        alert(
+          'There was an issuse with your payment.Please make sure you provided the given credit card'
+        );
+      });
   };
 
   return (
